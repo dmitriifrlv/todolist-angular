b10fdbf203dd0207128f36807907b8188a73329e
Dmitrii Frolov <dmitriifrolov@Dmitriis-MacBook-Pro.local>
2021-03-21 19:00:47 +0500

    initial commit

--- src/app/components/create-new-task/create-new-task.component.ts
+++ src/app/components/create-new-task/create-new-task.component.ts
@@ -11,30 +11,42 @@ import { Task } from '../../Models/Task'
   styleUrls: ['./create-new-task.component.css']
 })
 export class CreateNewTaskComponent {
-  @Input() tasks
-  @Input() selectedList:List
+  @Input() tasks;
+  @Input() selectedList:List;
   @Output() taskCreated = new EventEmitter();
-  requiredLetters:string=''
-  showAll:Boolean = true
+  requiredLetters:string='';
+  showAll:Boolean = true;
 
   taskTitleInput = new FormGroup({
     taskTitle: new FormControl('', [
       Validators.compose([Validators.minLength(1), Validators.required])
     ])
-  })
+  });
 
   constructor(private listsService:TodosService) { }
-
-  test(){
-    console.log(this.tasks) 
-  }
 
   taskFilter = (tasks)=>{
     this.showAll===true 
     ? this.tasks=tasks.filter((task:Task)=>task.description.includes(this.requiredLetters))
     : this.tasks=tasks.filter((task:Task)=>task.isCompleted===false && task.description.includes(this.requiredLetters))
-  }
+  };
+  completedTasksNumber = () =>this.tasks.filter((task:Task)=>task.isCompleted===true).length;
+  getTasks = () =>this.listsService.getTasks(this.selectedList.id).subscribe((tasks)=>this.tasks = tasks.filter((task:Task)=>task.description.includes(this.requiredLetters)))
+  myObserver = {
+    next: ()=>{
+      const nestedObserver = {
+        next:(tasks)=>{this.tasks=tasks},
+        complete: ()=>{
+          this.selectedList.numberOfCompletedTasks=this.completedTasksNumber()
+          this.selectedList.numberOfAllTasks=this.tasks.length
+          this.listsService.listUpdate(this.selectedList.id,this.selectedList).subscribe(()=>this.taskFilter(this.tasks))
+        }
+      }
+      this.listsService.getTasks(this.selectedList.id).subscribe(nestedObserver)},
+    complete: ()=>{this.tasks=this.taskFilter(this.tasks)}
+  };
 
+  //features:
   onAddTask(){
     if (this.taskTitleInput.valid) {
       const task = {
@@ -43,66 +55,33 @@ export class CreateNewTaskComponent {
         isCompleted: false,
         listsId:this.selectedList.id
       }
-      const myObserver = {
-        next:  ()=>this.listsService.getTasks(this.selectedList.id).subscribe((task)=>this.taskFilter(task)),
-        error: (err:string) => console.error('Observer got an error: ' + err),
-      };
-      this.listsService.addANewTask(task).subscribe(myObserver)
+      this.listsService.addANewTask(task).subscribe(()=>this.listsService.getTasks(this.selectedList.id).subscribe((task)=>this.taskFilter(task)))
       this.selectedList.numberOfAllTasks+=1
       this.listsService.listUpdate(this.selectedList.id,this.selectedList).subscribe()
       this.taskTitleInput.setValue({taskTitle:''})
     } 
   }
-  completedTasksNumber = () =>this.tasks.filter(x=>x.isCompleted===true).length;
+  
   onDeleteATask(id:string){
-    const myObserver = {
-      next: ()=>{
-        const nestedObserver = {
-          next:(tasks)=>{this.tasks=tasks},
-          complete: ()=>{
-            this.selectedList.numberOfCompletedTasks=this.completedTasksNumber()
-            this.selectedList.numberOfAllTasks=this.tasks.length
-            this.listsService.listUpdate(this.selectedList.id,this.selectedList).subscribe(()=>this.taskFilter(this.tasks))
-          }
-        }
-        this.listsService.getTasks(this.selectedList.id).subscribe(nestedObserver)},
-      complete: ()=>{this.tasks=this.taskFilter(this.tasks)}
-    };
-    this.listsService.deleteATask(id).subscribe(myObserver)
+    this.listsService.deleteATask(id).subscribe(this.myObserver)
   }
 
-  onUpdateATask(updatedTask) {
-    const myObserver = {
-      next: ()=>{
-        const nestedObserver = {
-          next:(tasks)=>{this.tasks=tasks},
-          complete: ()=>{
-            this.selectedList.numberOfCompletedTasks=this.tasks.filter(x=>x.isCompleted===true).length;
-            this.listsService.listUpdate(this.selectedList.id,this.selectedList).subscribe(()=>this.taskFilter(this.tasks))
-          }
-        }
-        this.listsService.getTasks(this.selectedList.id).subscribe(nestedObserver)},
-      complete: ()=>{this.tasks=this.taskFilter(this.tasks)}
-    };
-    this.listsService.toggleCompleted(updatedTask.id,updatedTask).subscribe(myObserver)
+  onUpdateATask(updatedTask:Task) {
+    this.listsService.toggleCompleted(updatedTask.id,updatedTask).subscribe(this.myObserver)
   }
 
   uncompletedFilter(){
     this.showAll=false
-    this.tasks=this.tasks.filter(x=>x.isCompleted===false && x.description.includes(this.requiredLetters)) 
+    this.tasks=this.tasks.filter((task:Task)=>task.isCompleted===false && task.description.includes(this.requiredLetters)) 
   }
 
   showAllTasks(){
     this.showAll=true;
-    this.listsService.getTasks(this.selectedList.id).subscribe((tasks)=>{
-      this.tasks = tasks.filter(x=>x.description.includes(this.requiredLetters))
-    })
+    this.getTasks()
   }
-
+  
   searchByLetters(letters:string){
     this.requiredLetters=letters
-    this.listsService.getTasks(this.selectedList.id).subscribe((tasks)=>{
-      this.tasks = tasks.filter(task=>task.description.includes(this.requiredLetters))
-    })
+    this.getTasks()
   }
 }
